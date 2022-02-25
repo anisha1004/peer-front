ab0f10a59b505d31d31c0eccc546a2043a1a51a6
anisha1004 <54628217+anisha1004@users.noreply.github.com>
2022-02-25 23:24:21 +0530

    home

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Sidebar from "./compnents/Sidebar";
+import Home from "./pages/Home";
 
 function App() {
 	return (
@@ -7,7 +8,9 @@ function App() {
 			<div className='app-left'>
 				<Sidebar />
 			</div>
-			<div className='app-right'></div>
+			<div className='app-right'>
+				<Home />
+			</div>
 		</div>
 	);
 }
